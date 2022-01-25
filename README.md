212a6f5cf44d6730bdfb2738b54b2e495e0c2511
szabikr <szabikr.dev@gmail.com>
2022-01-25 10:07:34 +0000

    initial setup: development environment with nodemon

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # move-to-done
 
+- `$ yarn install`
+- `$ yarn start`
+
+### Development environment
+
+- `$ yarn start:dev`
+
 ### Inspiration
 
 - [MERN Stack](https://www.mongodb.com/mern-stack)
--- server/app.js
+++ server/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Move to Done app!')
 })
 
+app.get('/todos', (req, res) => {
+  res.send('these will be the todos')
+})
+
 app.listen(port, () => {
   console.log(`Move to Done app listening on port ${port}`)
 })
