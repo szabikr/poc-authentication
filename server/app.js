80ec6f1286a04882ad73e84ef412d879b9f3ca0e
szabikr <szabikr.dev@gmail.com>
2022-01-25 10:47:32 +0000

    server: using uuid and defining mock todo list

--- server/app.js
+++ server/app.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello from Move to Done app!')
+app.use(express.json())
+
+let todos = [
+  {
+    id: '674b1934-dbaa-4679-88ce-513439e581be',
+    content: 'that',
+  },
+  {
+    id: '9c985402-2bcc-4f86-a7ac-fee7e40d3bfc',
+    content: 'this',
+  }, 
+  {
+    id: '5557295e-3e5f-4b7e-aec2-76b4815e1f59',
+    content: 'and that one'
+  }
+]
+
+app.get('/api/todos', (req, res) => {
+  res.send(todos)
 })
 
-app.get('/todos', (req, res) => {
-  res.send('these will be the todos')
+app.post('/api/todo', (req, res) => {
+  console.log('Request Body:', req.body)
+  const id = uuidv4()
+  res.json({ id: id })
+})
+
+app.get('/', (req, res) => {
+  res.send('<body><h1>Move to Done</h1><p>Frontend application coming soon...</p></body>')
 })
 
 app.listen(port, () => {
