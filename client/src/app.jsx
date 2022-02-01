73eb8d4e6ddc6b8edb261fb7d0ae1f4b83fcc0f8
szabikr <szabikr.dev@gmail.com>
2022-02-01 09:46:26 +0000

    client: configuring react front-end project

---
+++ client/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react' 
+import styles from './styles/app.module.css'
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Move to Done</h1>
+      <div className={styles.subtitle}>a <strong>szabi.space</strong> development</div>
+    </div>
+  )
+}
