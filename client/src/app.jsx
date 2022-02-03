0059d4e070ba5422bbb971488a631aee4792d22c
szabikr <szabikr.dev@gmail.com>
2022-02-03 10:24:18 +0000

    Client: Configuring material UI and setting darkmode

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react' 
+import Button from '@mui/material/Button'
 import styles from './styles/app.module.css'
 
 export default function App() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Move to Done</h1>
-      <a href="/todos" className="primary">Login</a>
-      <div className={styles.subtitle}>a <strong>szabi.space</strong> development</div>
+      <Button variant="contained">Login</Button>
+      <div>a <strong>szabi.space</strong> development</div>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 import App from './app'
 import './styles/global.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.Fragment>,
+  document.getElementById('root')
+)
