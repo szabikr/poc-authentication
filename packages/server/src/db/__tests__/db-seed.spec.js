ef02b6eb4782b0e45f658f3506a724bc00c0621c
szabikr <szabikr.dev@gmail.com>
2022-02-13 17:19:27 +0000

    MTD-17: Adding jest dependency, scripts and writing first test

---
+++ packages/server/src/db/__tests__/db-seed.spec.js
@@ -0,0 +1,18 @@
+const { myTodos } = require('../db-seed')
+
+it('should return the correct db seed', () => {
+  expect(myTodos).toEqual([
+    {
+      id: '674b1934-dbaa-4679-88ce-513439e581be',
+      content: 'that',
+    },
+    {
+      id: '9c985402-2bcc-4f86-a7ac-fee7e40d3bfc',
+      content: 'this',
+    },
+    {
+      id: '5557295e-3e5f-4b7e-aec2-76b4815e1f59',
+      content: 'and that one',
+    },
+  ])
+})
