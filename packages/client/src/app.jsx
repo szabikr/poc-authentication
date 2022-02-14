9a388cb45e610fc2b69f30f551a6600777f28cad
szabikr <szabikr.dev@gmail.com>
2022-02-14 11:29:50 +0000

    moves header and footer to components folder

--- packages/client/src/app.jsx
+++ packages/client/src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '@mui/material/Button'
-import Header from './header'
-import Footer from './footer'
+import Header from './components/header'
+import Footer from './components/footer'
 
 export default function App() {
   return (
