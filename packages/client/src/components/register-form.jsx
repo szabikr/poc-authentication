a7cf2483e2c32e82e76ca23a58c96fcfe50ad50f
szabikr <szabikr.dev@gmail.com>
2022-02-15 18:19:09 +0000

    MTD-11: user registration form and basic validation logic

--- packages/client/src/components/register-form.jsx
+++ packages/client/src/components/register-form.jsx
@@ -1,14 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Stack, TextField, Button } from '@mui/material'
 
 export default function RegisterForm() {
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+  const [confirmPassword, setConfirmPassword] = useState({
+    value: '',
+    error: '',
+  })
+
+  const handleRegisterOnClick = () => {
+    setEmail({ ...email, error: '' })
+    setPassword({ ...password, error: '' })
+    setConfirmPassword({ ...confirmPassword, error: '' })
+
+    if (email.value === '') {
+      setEmail({ ...email, error: 'Enter your email address' })
+    }
+
+    if (password.value === '') {
+      setPassword({ ...password, error: 'Enter your password' })
+    }
+    if (confirmPassword.value === '') {
+      setConfirmPassword({ ...confirmPassword, error: 'Confirm your password' })
+    }
+
+    if (password.value !== confirmPassword.value) {
+      return
+    }
+
+    console.log('we are ready to register with')
+    console.log(`email: ${email.value}`)
+    console.log(`password: ${password.value}`)
+    console.log(`confirmPassword: ${confirmPassword.value}`)
+  }
+
   return (
     <Container maxWidth="sm">
       <Stack spacing={2}>
-        <TextField id="email" label="Email" variant="outlined" />
-        <TextField id="password" label="Password" variant="outlined" />
-        <TextField id="password" label="Confirm password" variant="outlined" />
-        <Button variant="contained">Register</Button>
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          value={email.value}
+          onChange={(event) =>
+            setEmail({ ...email, value: event.target.value })
+          }
+          error={email.error !== ''}
+          helperText={email.error}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="outlined"
+          value={password.value}
+          onChange={(event) =>
+            setPassword({ ...password, value: event.target.value })
+          }
+          error={password.error !== ''}
+          helperText={password.error}
+        />
+        <TextField
+          id="password"
+          label="Confirm password"
+          variant="outlined"
+          value={confirmPassword.value}
+          onChange={(event) =>
+            setConfirmPassword({
+              ...confirmPassword,
+              value: event.target.value,
+            })
+          }
+          error={confirmPassword.error !== ''}
+          helperText={confirmPassword.error}
+        />
+        <Button variant="contained" onClick={handleRegisterOnClick}>
+          Register
+        </Button>
       </Stack>
     </Container>
   )
