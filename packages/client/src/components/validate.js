24ebe08c7e722b7c4973a168bd3571d690712110
szabikr <szabikr.dev@gmail.com>
2022-02-17 15:24:55 +0000

    MTD-11: Password complexity validation on the frontend

--- packages/client/src/components/validate.js
+++ packages/client/src/components/validate.js
@@ -1,3 +1,19 @@
+function isPasswordComplex(password) {
+  const hasCorrectLength = password.length >= 8 && password.length <= 32
+  const hasUpperCase = /[A-Z]/.test(password)
+  const hasLowerCase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasNonalphas = /\W/.test(password)
+
+  return (
+    hasCorrectLength &&
+    hasUpperCase &&
+    hasLowerCase &&
+    hasNumber &&
+    hasNonalphas
+  )
+}
+
 export default function validate(email, password, confirmPassword) {
   let emailError = ''
   let passwordError = ''
@@ -17,6 +33,11 @@ export default function validate(email, password, confirmPassword) {
 
   if (password !== confirmPassword) {
     confirmPasswordError = 'Passwords must match'
+  }
+
+  if (!isPasswordComplex(password)) {
+    passwordError =
+      'Password must be 8-32 characters with one lowecase, uppercase, number and symbol character'
   }
 
   return {
