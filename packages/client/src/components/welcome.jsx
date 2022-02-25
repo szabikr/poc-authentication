51387fdc10ef176f416d3133237ac9cf0a38d16d
szabikr <szabikr.dev@gmail.com>
2022-02-25 15:57:28 +0000

    Adds link on welcome to Register

--- packages/client/src/components/welcome.jsx
+++ packages/client/src/components/welcome.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack, Typography, Box, Button } from '@mui/material'
 
 export default function Welcome() {
-  return <h1>Give your day the structure that you want.</h1>
+  return (
+    <Stack>
+      <Typography variant="subtitle1" align="center">
+        Give your day the structure that you want.
+      </Typography>
+      <Box sx={{ mt: 4 }} textAlign="center">
+        <Link to="/register" style={{ textDecoration: 'none' }}>
+          <Button variant="contained">Create your account</Button>
+        </Link>
+      </Box>
+    </Stack>
+  )
 }
