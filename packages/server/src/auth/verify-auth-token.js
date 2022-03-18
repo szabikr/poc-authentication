656c1f3b9f448eefda7493c6ce7cf31ed245ef0d
szabikr <szabikr.dev@gmail.com>
2022-03-18 10:47:32 +0000

    MTD-41: Handles token expiry different to other token related issues

--- packages/server/src/auth/verify-auth-token.js
+++ packages/server/src/auth/verify-auth-token.js
@@ -1,17 +1,27 @@
 const jwt = require('jsonwebtoken')
 
+const ACCESS_DENIED_ERROR = 'Access Denied Error'
+const TOKEN_EXPIRED_ERROR = 'Token Expired Error'
+
 module.exports = function verifyAuthToken(req, res, next) {
   const token = req.header('auth-token')
 
   if (!token) {
-    return res.status(401).end('Access Denied')
+    return res.status(401).json({ error: ACCESS_DENIED_ERROR })
   }
 
   try {
     const result = jwt.verify(token, process.env.AUTH_TOKEN_SECRET)
     req.user = result.user
   } catch (err) {
-    return res.status(401).end('Access Denied')
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ error: TOKEN_EXPIRED_ERROR })
+    }
+
+    console.log('JWT Verification Error:')
+    console.log(JSON.stringify(err, null, 2))
+
+    return res.status(401).json({ error: ACCESS_DENIED_ERROR })
   }
 
   return next()
