9a894d7d13f5b0054bffaef2c5dcf7c68950d79f
szabikr <szabikr.dev@gmail.com>
2022-04-19 13:46:52 +0300

    Saves refresh token to localstorage

--- packages/client/src/auth/auth-context.jsx
+++ packages/client/src/auth/auth-context.jsx
@@ -1,11 +1,25 @@
-import React, { createContext, useCallback, useMemo, useState } from 'react'
+import React, {
+  createContext,
+  useCallback,
+  useMemo,
+  useState,
+  useEffect,
+} from 'react'
 import PropTypes from 'prop-types'
 
 export const AuthContext = createContext(null)
 
 function AuthContextProvider({ children }) {
   const [accessToken, setAccessToken] = useState(null)
-  const [refreshToken, setRefreshToken] = useState(null)
+  const [refreshToken, setRefreshToken] = useState(() => {
+    const savedRefreshToken = localStorage.getItem('refresh_token')
+    const initialValue = JSON.parse(savedRefreshToken)
+    return initialValue || null
+  })
+
+  useEffect(() => {
+    localStorage.setItem('refresh_token', JSON.stringify(refreshToken))
+  }, [refreshToken])
 
   const login = useCallback((response) => {
     setAccessToken(response.accessToken)
